7e1c529f55a7af2419495bd4e32b4009f6f80abf
tinovyatkin <tino@vtkn.io>
2017-03-21 19:50:25 -0400

    fixed use of persistent params

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const ua = require('universal-analytics');
 module.exports = (tid, options = {}) => {
   const { cookieName = '_ga' } = options;
   return (ctx, next) => {
-    const context = {
+    const persistentParams = {
       ua: ctx.get('user-agent'),
       dr: ctx.get('referrer'),
       uip: ctx.ip,
     };
-    if ('gclid' in ctx.query) context.gclid = ctx.query.gclid;
+    if ('gclid' in ctx.query) persistentParams.gclid = ctx.query.gclid;
 
     if (ctx.session && ctx.session.cid) {
-      ctx.state.visitor = ua(tid, ctx.session.cid, options, context);
+      Object.assign(persistentParams, ctx.session.ga_params || {});
+      ctx.state.visitor = ua(tid, ctx.session.cid, options, null, persistentParams);
       if (ctx.state.visitor) return next();
     }
 
@@ -23,10 +24,11 @@ module.exports = (tid, options = {}) => {
       } catch (e) {}
     }
 
-    ctx.state.visitor = ua(tid, cid, options, context);
+    ctx.state.visitor = ua(tid, cid, options, null, persistentParams);
 
     if (ctx.session) {
       ctx.session.cid = ctx.state.visitor.cid;
+      ctx.session.ga_params = persistentParams;
     }
 
     return next();
